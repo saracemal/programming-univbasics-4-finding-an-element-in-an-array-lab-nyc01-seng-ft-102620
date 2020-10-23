c9cc4489c7ec616acf06c2ec15e9f916791a35fc
Sara Cemal <saracemal@gmail.com>
2020-10-23 02:27:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,6 +4,7 @@ def find_element_index(array, value_to_find)
   while count < array.length do
     if array[count] == value_to_find
     puts array[count] == value_to_find
+  end
     count += 1
   end
   found_value_index
